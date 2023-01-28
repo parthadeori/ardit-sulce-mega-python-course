8e4029d0236c48b8064a474a1b26c8328c8350b4
Partha <acer.partha@gmail.com>
2023-01-29 03:08:56 +0530

    Day - 01 complete

---
+++ Day - 01/06-bug-fixing/main.py
@@ -0,0 +1,12 @@
+# The following code has a syntax error. Try to fix it by uncommenting it one by one
+
+## answers = ['Yes', 'No', 'Yes', 'No', 'No'
+
+## my_answer = input("What is your answer?")
+## answers = ['Yes', 'No', 'Yes' 'No' my_answer]
+
+## my_answer = input(What is your answer?)
+## answers = ['Yes', 'No', 'Yes', 'No', my_answer]
+
+## my_answer = input["What is your answer?"]
+## answers = ['Yes', 'No', 'Yes', 'No', my_answer]
