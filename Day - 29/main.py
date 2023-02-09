f3d5f48af9ef77b4b61c138a1a04d16468f86d35
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-09 22:14:01 +0530

    Update main.py file

--- Day - 29/main.py
+++ Day - 29/main.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask("Website")
 
-@app.route("/")
+@app.route("/home")
 
 def home():
-    return render_template(r"Day - 29\template\index.html")
+    return render_template("index.html")
+
+
+@app.route("/about/")
+
+def about():
+    return render_template("about.html")
 
 
 app.run(debug=True)
