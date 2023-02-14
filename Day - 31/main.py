ba08299450c084b5c6e17cb943011c6915298450
Coder Partha <80641510+parthadeori@users.noreply.github.com>
2023-02-14 07:56:20 +0530

    URL Endpoints for All Data and Annual Data

--- Day - 31/main.py
+++ Day - 31/main.py
@@ -1,3 +1,5 @@
+# URL Endpoints for All Data and Annual Data
+
 from flask import Flask, render_template
 import pandas as pd
 
@@ -21,6 +23,21 @@ def api(station, date):
         "date": date,
         "temperature": temperature
     }
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
